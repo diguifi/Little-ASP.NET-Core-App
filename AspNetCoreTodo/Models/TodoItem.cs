137d75a18a4e5110ab3fbba253df212e6a75fadd
diguifi <lumia-diguifi@hotmail.com>
2018-06-14 10:27:07 -0300

    MVC Basics chapter

---
+++ AspNetCoreTodo/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public bool IsDone { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
